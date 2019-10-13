3f281210a8a0d2e759c84042e98c70affb066178
sxckmymemes <50367957+sxckmymemes@users.noreply.github.com>
2019-10-13 19:22:51 +0300

    1

---
+++ js/radmir.js
@@ -0,0 +1,4 @@
+let radmir = {
+	launcher_url_xp: 'http://rdmr-delivery-network3.react.group/RADMIR_LAUNCHER_XP.exe',
+	launcher_url: 'http://rdmr-delivery-network3.react.group/RADMIR_LAUNCHER.exe'
+};
